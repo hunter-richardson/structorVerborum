49b6968c2af9a945c54aa9b2301655b38bdc016b
Hunter Richardson <hrichardson8@gmail.com>
2024-06-16 18:15:19 -0500

    Define front-end defaults

---
+++ src/figura.ts
@@ -0,0 +1,48 @@
+import { useFavicon } from '@vueuse/core';
+import { createApp } from 'vue';
+import { createVuetify } from 'vuetify';
+import { VDataTableHeaders } from 'vuetify/components';
+import App from './facies/app.vue';
+
+const app: App = createApp(App);
+
+useFavicon('/res/picta/favicon.png')
+
+export default createVuetify({
+  defaults: {
+    global: {
+      ripple: true,
+      VBtn: { rounded: 'pill' },
+      VNumberInput: {
+        controlVariant: 'stacked',
+        inset: true
+      }, VBtnToggle: {
+        rounded: true,
+        tile: true
+      }, VEmptyState: {
+        height: '80%',
+        width: '80%'
+      }, VDataTableHeader: {
+        align: 'start',
+        sortable: true,
+        sticky: true
+      }, VDialog: {
+        height: '80%',
+        width: '80%',
+        location: 'center',
+        scrim: false,
+        scrollable: true
+      }, VSnackbar: {
+        rounded: 'pill'
+      }, VChip: {
+        closeable: true,
+        filter: true,
+        pill: true,
+        rounded: true,
+        tile: true
+      }, VTabsWindowItem: {
+        transition: 'v-tab-transition'
+      }
+    }
+  }
+});
