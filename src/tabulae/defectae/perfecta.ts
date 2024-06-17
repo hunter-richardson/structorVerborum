8ce40e32aba537b5ab527ae22ae25a57d346d410
Hunter Richardson <hrichardson8@gmail.com>
2024-06-16 21:49:40 -0500

    Correct codefactor issues

--- src/tabulae/defectae/perfecta.ts
+++ src/tabulae/defectae/perfecta.ts
@@ -63,6 +63,7 @@ export default class TabulaPerfecta extends TabulaDefecta<Actus> {
           default:
             return null;
         }
+        break;
       default:
         return null;
     }
