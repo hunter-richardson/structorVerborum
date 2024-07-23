3a64366ab85277ad348908140a754169c10fc672
Hunter Richardson <hrichardson8@gmail.com>
2024-07-23 18:53:58 -0500

    Correct vite issues

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,21 @@
-import { fileURLToPath } from 'node:url';
-import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.ts';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { fileURLToPath, URL } from 'node:url';
+import checker from 'vite-plugin-checker';
+import VueDevTools from 'vite-plugin-vue-devtools';
+import { configDefaults, defineConfig } from 'vitest/config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
-  })
-)
+// https://vitejs.dev/config/
+/** @type {import ('vite').UserConfig} */
+export default defineConfig({
+  plugins: [
+    checker({ typescript: true }),
+    vue(), vueJsx(), VueDevTools(),
+  ], resolve: {
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+  }, test: {
+    environment: 'jsdom',
+    exclude: [ ...configDefaults.exclude, 'e2e/**' ],
+    root: fileURLToPath(new URL('./', import.meta.url))
+  }
+});
